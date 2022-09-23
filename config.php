02bfd600466fb6538cddcf03fb00a90fa069f933
Shrinit Poojary <shrinitpoojary1234@gmail.com>
2022-09-23 22:02:30 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$title = 'Railway management System';
+//E-Ticketing System For Railway
+$supervisor_name = "Alshifa";
+$developer_name = "Dhyeya s karkera ,Sahana Poojary";
+$developer_matric = "16/52HA016";
